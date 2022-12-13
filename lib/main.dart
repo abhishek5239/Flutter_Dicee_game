185bd7b6f44a008a1f9145c41fc8e5cf1edc3ea1
Abhishek Bhadane <abhishek.bhadane20@pccoepune.org>
2022-12-14 00:49:04 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,6 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -15,9 +17,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  // const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  void dicechange() {
+    dicecount1 = Random().nextInt(6) + 1;
+    dicecount2 = Random().nextInt(6) + 1;
+  }
+
+  int dicecount1 = 1;
+  int dicecount2 = 3;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Row(
+      children: [
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                dicechange();
+              });
+            },
+            style: ButtonStyle(
+                padding: MaterialStateProperty.all(EdgeInsets.all(20.0))),
+            child: Image.asset('images/dice$dicecount1.png'),
+          ),
+        ),
+        Expanded(
+            child: TextButton(
+          onPressed: () {
+            setState(() {
+              dicechange();
+            });
+          },
+          style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.all(20.0))),
+          child: Image.asset('images/dice$dicecount2.png'),
+        ))
+      ],
+    ));
   }
 }
+
+// class DicePage extends StatelessWidget {
+//
+// }
---
+++ lib/practice.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  // const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  Widget build(BuildContext context) {}
+}
+
+// class DicePage extends StatelessWidget {
+//
+// }
